10e2a2b0bf9948a12f18479d6fbe28c74eb2ba2f
Martin Osman <cppmember95@gmail.com>
2018-09-15 14:21:46 +0200

    Add files via upload

---
+++ KingICTExample/KingICTExample/KingICTExample/FlightControl.cs
@@ -0,0 +1,157 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KingICTExample
+{
+    class FlightControl
+    {
+        public string APIKey { get; set; }
+        public string RawJSONResponse { get; set; }
+
+       
+        public FlightControl(string apiKey)
+        {
+            if (String.IsNullOrWhiteSpace(apiKey)) throw new System.ArgumentException("API Key is require for all operations.");
+            this.APIKey = apiKey;
+        }
+
+
+       
+        public FlightResponse GetFlights(string qryOrigin, string qryDestination, DateTime DateLeave, DateTime DateReturn, int qryPeople, string qryCurrency, int qryResultsNum)
+        {
+            if (qryResultsNum > 250) throw new System.ArgumentException("Maximum number of results can't be over 250", "qryResultsNum");
+
+           // if (DateLeave.Date <= DateTime.Now.Date) throw new System.ArgumentException("Departure date needs to in the future", "DateLeave");
+            string qryDateLeave = DateLeave.ToString("yyyy-MM-dd");
+
+            string qryDateReturn = String.Empty;
+            if (DateReturn != null)
+            {
+                if (((DateTime)DateReturn).Date < DateLeave.Date) throw new System.ArgumentException("Return date can't be before departure date", "DateReturn");
+                qryDateReturn = DateReturn.ToString("yyyy-MM-dd");
+            }
+
+
+            ;
+            if (File.Exists($"{qryOrigin}-{qryDestination}-{qryDateLeave}-{qryDateReturn}-{qryPeople}-{qryCurrency}-{qryResultsNum}.json"))
+            {
+                using (StreamReader SR = new StreamReader($"{qryOrigin}-{qryDestination}-{qryDateLeave}-{qryDateReturn}-{qryPeople}-{qryCurrency}-{qryResultsNum}.json"))
+                {
+                    RawJSONResponse = SR.ReadToEnd();
+                    return JsonConvert.DeserializeObject<FlightResponse>(RawJSONResponse);
+                }
+            }
+            else
+            {
+                string TQryOorigin = qryOrigin.Trim();
+                string TQryDestination = qryDestination.Trim();
+                string TQryCurrency = qryCurrency.Trim();
+                string URL = "https://" + $@"api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey={APIKey}&origin={TQryOorigin}&destination={TQryDestination}&departure_date={qryDateLeave}&{((qryDateReturn == "") ? "" : $"return_date={qryDateReturn}")}&adults={qryPeople}&currency={TQryCurrency}&number_of_results={qryResultsNum}";
+              
+                HttpWebRequest Req = (HttpWebRequest)WebRequest.Create(URL);
+                Req.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+                using (HttpWebResponse Res = (HttpWebResponse)Req.GetResponse())
+                {
+                    using (Stream ResStream = Res.GetResponseStream())
+                    {
+                        using (StreamReader reader = new StreamReader(ResStream))
+                        {
+                            RawJSONResponse = reader.ReadToEnd();
+                            using (StreamWriter SW = new StreamWriter($"{TQryOorigin}-{TQryDestination}-{qryDateLeave}-{qryDateReturn}-{qryPeople}-{TQryCurrency}-{qryResultsNum}.json"))
+                            {
+                                SW.Write(RawJSONResponse);
+                            }
+                            
+                            return JsonConvert.DeserializeObject<FlightResponse>(RawJSONResponse);
+                       
+                        }
+                    }
+                }
+              
+            }
+            
+        }
+   
+
+      
+        public class FlightResponse
+        {
+            public string Currency { get; set; }
+            public List<FlightResults> Results { get; set; }
+        }
+
+    
+        public class FlightResults
+        {
+            public List<FlightItineraries> Itineraries { get; set; }
+            public FlightFare Fare { get; set; }
+        }
+
+       
+        public class FlightItineraries
+        {
+            public FlightXbound Outbound { get; set; }
+            public FlightXbound Inbound { get; set; }
+        }
+
+  
+        public class FlightXbound
+        {
+            public string Duration { get; set; }
+            public List<FlightFlights> Flights { get; set; }
+
+        }
+
+        public class FlightFlights
+        {
+            public DateTime Departs_at { get; set; }
+            public DateTime Arrives_at { get; set; }
+            public FlightTerminal Origin { get; set; }
+            public FlightTerminal Destination { get; set; }
+            public string Marketing_airline { get; set; }
+            public string Operating_airline { get; set; }
+            public string Flight_number { get; set; }
+            public string Aircraft { get; set; }
+            public FlightBookingInfo Booking_info { get; set; }
+        }
+
+        public class FlightTerminal
+        {
+            public string Airport { get; set; }
+            public string Terminal { get; set; }
+        }
+
+        public class FlightBookingInfo
+        {
+            public string Travel_class { get; set; }
+            public string Booking_code { get; set; }
+            public int Seats_remaining { get; set; }
+        }
+
+        public class FlightFare
+        {
+            public double Total_price { get; set; }
+            public FlightPricePerAdult Price_per_adult { get; set; }
+            public FlightRestrictions Restrictions { get; set; }
+        }
+
+        public class FlightPricePerAdult
+        {
+            public string Total_fare { get; set; }
+            public string Tax { get; set; }
+        }
+
+        public class FlightRestrictions
+        {
+            public bool Refundable { get; set; }
+            public bool Change_penalties { get; set; }
+        }
+    }
+}
---
+++ KingICTExample/KingICTExample/KingICTExample/MainWindow.xaml.cs
@@ -0,0 +1,176 @@
+﻿using AmadeusFlightAPI;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using static AmadeusFlightAPI.FlightControl;
+
+namespace KingICTExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            load_data();
+
+        }
+         
+        
+       
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            start();
+            
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+
+        private void load_data()
+        {
+            StreamReader sr = new StreamReader(@"IATACodes.txt");
+            string x = sr.ReadToEnd();
+            string[] y = x.Split('\n');
+            foreach (string s in y)
+            {
+                cmbOrigin.Items.Add(s);
+            }
+            StreamReader sr1 = new StreamReader(@"IATACodes.txt");
+            string x1 = sr1.ReadToEnd();
+            string[] y1 = x1.Split('\n');
+            foreach (string s in y1)
+            {
+                cmbDestination.Items.Add(s);
+            }
+            StreamReader sr2 = new StreamReader(@"numberOdAdults.txt");
+            string x2 = sr2.ReadToEnd();
+            string[] y2 = x2.Split('\n');
+            foreach (string s in y2)
+            {
+                cmbAdults.Items.Add(s);
+            }
+            StreamReader sr3 = new StreamReader(@"currencies.txt");
+            string x3 = sr3.ReadToEnd();
+            string[] y3 = x3.Split('\n');
+            foreach (string s in y3)
+            {
+                cmbCurrency.Items.Add(s);
+            }
+            StreamReader sr4 = new StreamReader(@"numberOfResults.txt");
+            string x4 = sr4.ReadToEnd();
+            string[] y4 = x4.Split('\n');
+            foreach (string s in y4)
+            {
+                cmbNumberOfResult.Items.Add(s);
+            }
+        }
+
+
+
+       
+
+        private void datePickStart_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DateTime DateLeave = Convert.ToDateTime(datePickStart.SelectedDate.ToString());
+            if (DateLeave.Date < DateTime.Now.Date)
+            {
+                MessageBox.Show("Departure date needs to be in the future!");
+                datePickStart.SelectedDate = DateTime.Today;
+            }
+        }
+
+        private void datePickEnd_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DateTime DateReturn = Convert.ToDateTime(datePickEnd.SelectedDate.ToString());
+            DateTime DateLeave = Convert.ToDateTime(datePickStart.SelectedDate.ToString());
+            if (DateReturn.Date <= DateLeave.Date)
+            {
+                MessageBox.Show("Return date needs to be after departure date!");
+               
+            }
+        }
+
+        private void start()
+        {
+            string QryAPIKey = "JXQ3tAn4sVXSn7ahn7UJSEyQOcDIUxpd";
+            var Controls = new FlightControl(QryAPIKey);
+            if (cmbOrigin.SelectedItem == null || cmbDestination.SelectedItem==null || datePickStart.SelectedDate==null || datePickEnd.SelectedDate == null)
+            {
+
+
+                MessageBox.Show("Please fill all fields!");
+            }
+            else
+            {
+
+                string QryOrigin = cmbOrigin.SelectedItem.ToString();
+                string QryDestination = cmbDestination.SelectedItem.ToString();
+                DateTime DateLeave = Convert.ToDateTime(datePickStart.SelectedDate.ToString());
+                DateTime DateReturn = Convert.ToDateTime(datePickEnd.SelectedDate.ToString());
+
+                int QryPeople = Convert.ToInt32(cmbAdults.SelectedItem.ToString());
+                if (QryPeople == 0)
+                {
+                    QryPeople = 1;
+                }
+                else QryPeople = QryPeople;
+
+                string QryCurrency = cmbCurrency.SelectedItem.ToString();
+                if (QryCurrency == "")
+                {
+                    QryCurrency = "USD";
+                }
+                else QryCurrency = QryCurrency;
+
+                int QryResultsNum = Convert.ToInt32(cmbNumberOfResult.SelectedItem.ToString());
+
+                if (QryResultsNum == 0)
+                {
+                    QryResultsNum = 5;
+                }
+                else QryResultsNum = QryResultsNum;
+
+                var Flights = Controls.GetFlights(QryOrigin, QryDestination, DateLeave, DateReturn, QryPeople, QryCurrency, QryResultsNum);
+                BindingOperations.ClearAllBindings(lbResults);
+                foreach (var flight in Flights.Results)
+                {
+                    
+
+                    lbResults.Items.Add(new
+                    {
+                        
+                        Currency = Flights.Currency,
+
+                        Origin = flight.Itineraries[0].Outbound.Flights[0].Origin.Airport,
+                        Destination = flight.Itineraries[0].Outbound.Flights[flight.Itineraries[0].Inbound.Flights.Count - 1].Destination.Airport,
+                        LeaveDate = flight.Itineraries[0].Outbound.Flights[0].Departs_at,
+                        ReturnDate = flight.Itineraries[0].Inbound.Flights[0].Departs_at,
+                        Transfers = flight.Itineraries[0].Outbound.Flights.Count - 1,
+                        Fare = flight.Fare.Total_price,
+                        Passangers = QryPeople
+
+
+                    });
+                }
+
+
+
+
+
+              
+            }
+        }
+    }
+    
+}
